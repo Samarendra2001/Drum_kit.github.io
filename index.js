3a54b8990a7c89f1f010c7753ca72f39cb0af5c6
Samrendra Barick <98378996+Samarendra2001@users.noreply.github.com>
2023-11-07 13:25:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,27 +18,27 @@ function makesound(key) {
       var audio1 = new Audio("sounds/tom-1.mp3");
       audio1.play();
       break;
-    case "u":
+    case "a":
       var audio2 = new Audio("sounds/tom-3.mp3");
       audio2.play();
       break;
-    case "b":
+    case "m":
       var audio3 = new Audio("sounds/tom-4.mp3");
       audio3.play();
       break;
-    case "h":
+    case "a":
       var audio4 = new Audio("sounds/snare.mp3");
       audio4.play();
       break;
-    case "a":
+    case "r":
       var audio5 = new Audio("sounds/kick-bass.mp3");
       audio5.play();
       break;
-    case "m":
+    case "1":
       var audio6 = new Audio("sounds/crash.mp3");
       audio6.play();
       break;
-    case "B":
+    case "2":
       var audio7 = new Audio("sounds/tom-2.mp3");
       audio7.play();
       break;
